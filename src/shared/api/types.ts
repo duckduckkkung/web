711c0f1b062c342ea32686b9388f1a6203163e9e
ICe1BotMaker <ice1github@gmail.com>
2025-12-24 21:32:09 +0900

    [remove] 기존 레거시 api-client 관련 코드 제거

--- src/shared/api/types.ts
+++
@@ -1,7 +0,0 @@
-// export type DefaultResponse<T> = Promise<{
-//     status: number;
-//     errorType: string;
-//     message: string;
-//     data: T;
-// }>;
-export type DefaultResponse<T> = Promise<T>;
